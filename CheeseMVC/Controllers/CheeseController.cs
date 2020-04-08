96271ea0a3e6c0aa69dad3c4870e352ce4039feb
AJSTL <ashley.meyer@gmail.com>
2020-04-07 22:54:19 -0500

    Class9 prep (part 1)

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -1,98 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using CheeseMVC.Models;
-
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using CheeseMVC.ViewModels;
 
 namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
-
-        //static private List<string> Cheeses = new List<string>();
-
-        /*Add a description field to the application. This will involve:
-        Modifying the static cheeses list to be a dictionary with key/value pairs that correspond to the name and description.
-        Adding a new form field in Cheese/Add.cshtml to allow for submission of the description.
-        Modify the NewCheese action to insert the description of the new cheese into the cheeses dictionary.
-        Display the description field in the Cheese/Index.cshtml view template.*/
-
-        
-
-
         //calls the index method of our cheese controller
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.Cheeses = CheeseData.GetAll();
-            //ViewData["cheese"] = CheeseData.GetAll();
+            List<Cheese> cheeses = CheeseData.GetAll(); // best way to bring in Cheeses directly, without using ViewBag
 
-            return View();
+            //ViewBag.Cheeses = CheeseData.GetAll();
+            //ViewData["cheese"] = CheeseData.GetAll(); // same as above
+
+            return View(cheeses);
         }
 
 
         public IActionResult Add()
         {
-            return View();
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+
+            return View(addCheeseViewModel);
         }
 
-        //public IActionResult Duplicate(string name)
-        //{
-       //     Content("{0} is a duplicate cheese. Please enter a different cheese.", name); //does not work
-      //   return Redirect("/Cheese"); 
-      //  }
       
 
         [HttpPost]
-        [Route("/Cheese/Add")]
-        public IActionResult NewCheese(Cheese newCheese)
+        public IActionResult Add(AddCheeseViewModel addCheeseViewModel)
         {
-            /*
-            Cheese newCheese = new Cheese
+            if (ModelState.IsValid) // use the viewmodel to validate
             {
-                Description = description,
-                Name = name
-            };
-
-             ^^ shorthand, same as:
-                Cheese newCheese = new Cheese(); 
-                newCheese.Description=description; 
-                newCheese.Name=name; */
-
-            // Cheeses.Add(new Cheese(name,description)); // code above does the same but without a constructor; uses the default in the model
-
-            //add the new cheese
-            CheeseData.Add(newCheese);
-
-            return Redirect("/Cheese");
-            
-        }
-
-        /*public IActionResult Remove()
-        {
-            ViewBag.cheeses = Cheeses;
-            return View();
-        }*/
-
-        /*
-        [HttpPost]
-        [Route("/Cheese/Remove")]
-        public IActionResult RemoveCheese(string[] cheeseName)
-        {
-            {
-                // remove the old cheese
-                foreach (Cheese cheese in cheeseName)
+                //add the new cheese
+                Cheese newCheese = new Cheese
                 {
-                    Cheeses.Remove(cheese);
-                }
+                    Name = addCheeseViewModel.Name,
+                    Description = addCheeseViewModel.Description
+                };
+
+                CheeseData.Add(newCheese);
                 return Redirect("/Cheese");
             }
+
+            return View(addCheeseViewModel);
+
         }
-        */
+
+        
 
         public IActionResult Remove()
         {
---
+++ CheeseMVC/ViewModels/AddCheeseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCheeseViewModel
+    {
+        // to represent all of the data associated with the "Add" View
+        [Required]
+        [Display(Name="Cheese Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage="You must give the cheese a description.")]
+        public string Description { get; set; }
+    }
+}
