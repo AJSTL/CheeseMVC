d935411a25c9e95b450980e33fe6548c7d2cf22f
AJSTL <ashley.meyer@gmail.com>
2020-04-07 23:17:23 -0500

    class 9 prep part 2 complete

---
+++ CheeseMVC/Models/CheeseType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public enum CheeseType // enum = a class with a specific, finite set of values
+    {
+        Hard, Soft, Fake
+    }
+}
